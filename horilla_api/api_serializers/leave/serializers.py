2ff562ce410d3783391da5cc728af3d14c417334
Iman <sudiman.ade@gmail.com>
2026-03-15 16:03:49 +0700

    Update serializers.py

--- horilla_api/api_serializers/leave/serializers.py
+++ horilla_api/api_serializers/leave/serializers.py
@@ -115,37 +115,6 @@ class GetAvailableLeaveTypeSerializer(serializers.ModelSerializer):
             return None
 
 
-class GetAvailableLeaveTypeSerializer(serializers.ModelSerializer):
-    leave_type_id = serializers.SerializerMethodField()
-    icon = serializers.SerializerMethodField()
-    total_leave_days = serializers.SerializerMethodField()
-
-    class Meta:
-        model = AvailableLeave
-        fields = [
-            "id",
-            "leave_type_id",
-            "icon",
-            "available_days",
-            "carryforward_days",
-            "total_leave_days",
-        ]
-
-    def get_leave_type_id(self, obj):
-        if obj.leave_type_id:
-            return LeaveTypeAllGetSerializer(obj.leave_type_id).data
-        return None
-
-    def get_icon(self, obj):
-        try:
-            return obj.leave_type_id.icon.url
-        except:
-            return None
-
-    def get_total_leave_days(self, obj):
-        return obj.available_days + obj.carryforward_days
-
-
 class userLeaveRequestGetAllSerilaizer(serializers.ModelSerializer):
     leave_type_id = serializers.SerializerMethodField()
 
