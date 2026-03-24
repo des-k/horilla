dac8c3e630251737ff40a7d8f3afc78ffe387332
Iman <sudiman.ade@gmail.com>
2026-03-25 01:07:15 +0700

    add test settings

---
+++ horilla/test_settings.py
@@ -0,0 +1,14 @@
+from .settings import *  # noqa
+
+
+class DisableMigrations(dict):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
