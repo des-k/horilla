0ac0f90b211ad5e920c8758bbfe427d808e27c07
Iman <sudiman.ade@gmail.com>
2026-03-28 21:26:39 +0700

    Update __init__.py

--- leave/__init__.py
+++ leave/__init__.py
@@ -1,1 +1,8 @@
-from . import scheduler
+import os
+import sys
+
+if os.environ.get("HORILLA_DISABLE_SCHEDULERS") != "1" and not any(
+    cmd in sys.argv
+    for cmd in ["makemigrations", "migrate", "compilemessages", "flush", "shell", "test"]
+):
+    from . import scheduler
